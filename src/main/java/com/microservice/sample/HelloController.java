95213741884b27206290aae478b4d95669aa454e
ninjasul <ninjasul@gmail.com>
2021-12-28 13:44:35 +0900

    Initial commit

---
+++ src/main/java/com/microservice/sample/HelloController.java
@@ -0,0 +1,16 @@
+package com.microservice.sample;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        log.info("Hello Microservice");
+        return "Hello Microservice";
+    }
+}
